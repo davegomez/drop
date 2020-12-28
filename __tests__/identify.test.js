2acc0c02da868a891ebaad8ddc71f1693cf73255
David Gomez <dg.dave@hey.com>
2020-12-28 11:29:05 -0500

    Write and test the identify to manage the ids

---
+++ __tests__/identify.test.js
@@ -0,0 +1,27 @@
+/* Copyright (C) 2020 Drop Authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import '@testing-library/jest-dom';
+import identify from '../src/identify';
+
+test('keeps the current id and returns it', () => {
+  const element = document.createElement('button');
+  element.setAttribute('id', 'foo');
+  const id = identify(element);
+
+  expect(element).toHaveAttribute('id', 'foo');
+  expect(id).toEqual('foo');
+});
+
+test('creates an id, marks the element, and returns the id', () => {
+  const element = document.createElement('button');
+  const id = identify(element);
+
+  expect(element).toHaveAttribute('id', id);
+  expect(element).toHaveAttribute('data-drop-id');
+  expect.stringContaining('drop-');
+});
---
+++ src/identify.ts
@@ -0,0 +1,37 @@
+/* Copyright (C) 2020 Drop Authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * Uses an id to identify the Dropdown element used to open and close the menu.
+ * If the element already has an id, it returns the same id; if it doesn't,
+ * generates a new id, marks the element with a data attribute, and returns the
+ * created id.
+ * @internal
+ *
+ * @remarks
+ *
+ * It uses the data attribute to identify if the element has an id created by
+ * this library. This attribute allows us to remove the id on destroying safely.
+ *
+ * @param element - Element used to open and close the menu.
+ * @param max - Maximum value to create the random id.
+ * @returns The element id.
+ */
+const identify = (element: Element, max = 99999) => {
+  const id =
+    element.getAttribute('id') ||
+    `drop-${Math.floor(Math.random() * Math.floor(max))}`;
+
+  if (!element.getAttribute('id')) {
+    element.setAttribute('data-drop-id', '');
+    element.setAttribute('id', id);
+  }
+
+  return id;
+};
+
+export default identify;
