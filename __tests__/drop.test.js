6be282fb3ca84c68e2f2baa9a734978d83748c41
David Gomez <dg.dave@hey.com>
2020-12-28 14:19:33 -0500

    Update copyright text

--- __tests__/drop.test.js
+++ __tests__/drop.test.js
@@ -1,8 +1,9 @@
-/* Copyright (C) 2020 Drop Authors.
+/*
+ * Copyright (c) 2020 Drop Authors.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 import drop from '../src/drop';
