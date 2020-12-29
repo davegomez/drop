9aa515e59cc55576b75a30bd33c25668aee9521e
David Gomez <dg.dave@hey.com>
2020-12-29 14:07:23 -0500

    Create constants file

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Drop Authors.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export const DATA_DROP_CONTAINER = 'data-drop-container';
+export const DATA_DROP_TRIGGER = 'data-drop-trigger';
+export const DATA_DROP_MENU = 'data-drop-menu';
+export const DATA_DROP_ACTION = 'data-drop-action';
