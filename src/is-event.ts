97f02dbaed7fad7da4d3c190592f16942c9fcfa1
David Gomez <dg.dave@hey.com>
2020-12-29 14:08:33 -0500

    Change the way to export the is-event functions

--- src/is-event.ts
+++ src/is-event.ts
@@ -6,19 +6,24 @@
  * obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-const isEnter = ({ code, keyCode, type }: KeyboardEvent): boolean =>
+export const isEnter = ({ code, keyCode, type }: KeyboardEvent): boolean =>
   type === 'keydown' && (code === 'Enter' || keyCode === 13);
 
-const isSpace = ({ code, keyCode, type }: KeyboardEvent): boolean =>
+export const isSpace = ({ code, keyCode, type }: KeyboardEvent): boolean =>
   type === 'keyup' && (code === 'Space' || keyCode === 32);
 
-const isEscape = ({ code, keyCode, type }: KeyboardEvent): boolean =>
+export const isEscape = ({ code, keyCode, type }: KeyboardEvent): boolean =>
   type === 'keydown' && (code === 'Escape' || keyCode === 27);
 
-const isTab = ({ code, keyCode, shiftKey, type }: KeyboardEvent): boolean =>
+export const isTab = ({
+  code,
+  keyCode,
+  shiftKey,
+  type,
+}: KeyboardEvent): boolean =>
   type === 'keydown' && !shiftKey && (code === 'Tab' || keyCode === 9);
 
-const isShiftTab = ({
+export const isShiftTab = ({
   code,
   keyCode,
   shiftKey,
@@ -26,18 +31,8 @@ const isShiftTab = ({
 }: KeyboardEvent): boolean =>
   type === 'keydown' && shiftKey && (code === 'Tab' || keyCode === 9);
 
-const isArrowUp = ({ code, keyCode, type }: KeyboardEvent): boolean =>
+export const isArrowUp = ({ code, keyCode, type }: KeyboardEvent): boolean =>
   type === 'keydown' && (code === 'ArrowUp' || keyCode === 38);
 
-const isArrowDown = ({ code, keyCode, type }: KeyboardEvent): boolean =>
+export const isArrowDown = ({ code, keyCode, type }: KeyboardEvent): boolean =>
   type === 'keydown' && (code === 'ArrowDown' || keyCode === 40);
-
-export {
-  isEnter,
-  isSpace,
-  isEscape,
-  isTab,
-  isShiftTab,
-  isArrowUp,
-  isArrowDown,
-};
